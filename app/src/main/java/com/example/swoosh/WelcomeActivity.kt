2f46f65e3e9115dc9c0d3b73177d2a3736ffc877
SHARMA JI <sharmaanshul9650@gmail.com>
2022-01-04 10:52:58 +0530

    lesson 4

--- app/src/main/java/com/example/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/example/swoosh/WelcomeActivity.kt
@@ -1,18 +1,18 @@
 package com.example.swoosh
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
-class WelcomeActivity : AppCompatActivity() {
+class WelcomeActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
 
         //Get Started Button
         val getStartedBtn: Button = findViewById(R.id.getStartedBtn)
-        getStartedBtn.setOnClickListener{
+        getStartedBtn.setOnClickListener {
             val leagueIntent = Intent(this, LeagueActivity::class.java)
             startActivity(leagueIntent)
         }
